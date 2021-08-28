d9eee3cdcfeded8591d9e709d18bec631e891aa8
Nathan <hajinopro@gmail.com>
2021-08-28 22:47:02 +0900

    Initial Commit

---
+++ NathanTIL/NathanTILApp.swift
@@ -0,0 +1,17 @@
+//
+//  NathanTILApp.swift
+//  NathanTIL
+//
+//  Created by Nathan on 2021/08/28.
+//
+
+import SwiftUI
+
+@main
+struct NathanTILApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
